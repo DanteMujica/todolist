0ccadc0bccf27737e9723066978a9b1d98a849e9
DanteMujica <mujica7group@gmail.com>
2020-07-07 06:31:41 +0900

    cloneメソッド追加

--- JQuery.js
+++ JQuery.js
@@ -1,4 +1,18 @@
+$('h1').hover(
+  function () {
+    $('.setsumei').fadeIn();
+  },
+  function () {
+    $('.setsumei').fadeOut();
+  }
+);
+const $list = $('#list');
+
 $('.btn').click(function () {
   let text = $('#text').val();
-  $('#output-text').text(text);
+  $('.output-item').text(text);
+  $list.clone().appendTo('#list');
+});
+$('.output-item').click(function () {
+  $(this).addClass('end');
 });
